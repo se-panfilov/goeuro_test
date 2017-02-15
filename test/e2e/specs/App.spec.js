cbe530ed3b207191e2c19a10675b3bb4d946ba73
Sergey Panfilov <se-panfilov@ya.ru>
2017-02-15 16:37:20 +0800

    some e2e

--- test/e2e/specs/App.spec.js
+++ test/e2e/specs/App.spec.js
@@ -1,14 +1,11 @@
-const sinon = require('sinon')
-const fetchMock = require('fetch-mock')
-const expect = require("chai").expect
-
 const elements = {
   submitBtn: 'button[type=submit]',
   form: 'form[id=search_form]',
   usernameInput: 'input[id=username]',
   isCorsCheckbox: 'input[id=is_cors]',
   reposList: 'ul[id=repos_list]',
-  notificationsBlock: 'div[id=notifications]'
+  notificationsBlock: 'div[id=notifications]',
+  listItems: 'ul#repos_list li'
 }
 
 function initPage (browser) {
@@ -22,69 +19,44 @@ function initPage (browser) {
 }
 
 module.exports = {
-  // 'default state': function (browser) {
-  //   browser = initPage(browser)
-  //   browser.expect.element(elements.form).to.be.present.before(1000)
-  //   browser.expect.element(elements.notificationsBlock).to.be.present.before(1000)
-  //
-  //   browser.expect.element(elements.form).to.be.a('form')
-  //   browser.expect.element(elements.submitBtn).to.be.a('button')
-  //   browser.expect.element(elements.usernameInput).to.be.a('input')
-  //   browser.expect.element(elements.isCorsCheckbox).to.be.a('input')
-  //
-  //   browser.expect.element(elements.reposList).to.be.a('ul')
-  //   // TODO (S.Panfilov) add elem's childs count === 0
-  //
-  //   browser.end()
-  // },
+  'default state': function (browser) {
+    browser = initPage(browser)
+    browser.expect.element(elements.form).to.be.present.before(1000)
+    browser.expect.element(elements.notificationsBlock).to.be.present.before(1000)
+
+    browser.expect.element(elements.form).to.be.a('form')
+    browser.expect.element(elements.submitBtn).to.be.a('button')
+    browser.expect.element(elements.usernameInput).to.be.a('input')
+    browser.expect.element(elements.isCorsCheckbox).to.be.a('input')
+
+    browser.expect.element(elements.reposList).to.be.a('ul')
+    browser.assert.elementCount(elements.listItems, 0)
+
+    browser.end()
+  },
   'get repos': function (browser) {
     browser = initPage(browser)
     browser.expect.element(elements.form).to.be.present.before(1000)
-
-    browser.expect.element(elements.reposList).to.be.a('ul')
-
-    // let server
-    const url = 'https://api.github.com/users/se-panfilov/repos'
-    const responseData = [
-      {name: 'adsdsd'}
-    ]
-
-    // browser.execute(() => {
-    //   server = sinon.fakeServer.create()
-    //   server.respondWith('GET', url, [
-    //     200, {'Content-Type': 'application/json'},
-    //     JSON.stringify(responseData),
-    //   ])
-    // })
-
-
-    browser.execute(() => {
-
-      var couchdb = nock('https://api.github.com')
-        .get('/users/se-panfilov/repos')
-        .reply(200,responseData)
-      })
+    browser.assert.elementCount(elements.listItems, 0)
 
     browser.click(elements.submitBtn)
-
-    // const _fetchMock = fetchMock.get(url, responseData)
-
-
-    // browser.execute(() => {
-    //   server.respond()
-    // })
-
     browser.pause(1000)
 
-    browser.assert.elementCount("ul#repos_list li", 12323)
+    browser.assert.elementCountMoreThan(elements.listItems, 0)
 
-    // expect(_fetchMock.called(url)).to.be.true
-    // _fetchMock.restore()
+    browser.end()
+  },
+  'unknown user': function (browser) {
+    browser = initPage(browser)
+    browser.expect.element(elements.form).to.be.present.before(1000)
+    browser.assert.elementCount(elements.listItems, 0)
 
-    // browser.execute(() => {
-    //   server.restore()
-    //   server = null
-    // })
+    browser.setValue(elements.usernameInput,'123213')
+    browser.click(elements.submitBtn)
+    browser.pause(1000)
+
+    browser.assert.elementCount(elements.listItems, 0)
+
     browser.end()
   }
 
